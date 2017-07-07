73b0b7bae1b26de23cea15068f78e44a2689d437
MyGit <1049881321@qq.com>
2017-07-07 08:46:26 +0800

    完成了商店的专题页面

---
+++ app/src/main/java/com/example/pc/imitationliangcang/ui/adapter/shopfragment/TopicFragmentRecyclerVIewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.pc.imitationliangcang.ui.adapter.shopfragment;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.pc.imitationliangcang.R;
+import com.example.pc.imitationliangcang.bean.TopicFragmentBean;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by PC on 2017/7/7.
+ */
+
+public class TopicFragmentRecyclerVIewAdapter extends RecyclerView.Adapter<TopicFragmentRecyclerVIewAdapter.TopicFragmentViewHolder> {
+
+    private final FragmentActivity mContext;
+    private final List<TopicFragmentBean.DataBean.ItemsBean> items;
+    private final LayoutInflater mLayoutInflater;
+
+    public TopicFragmentRecyclerVIewAdapter(FragmentActivity mContext, List<TopicFragmentBean.DataBean.ItemsBean> items) {
+        this.mContext = mContext;
+        this.items = items;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public TopicFragmentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new TopicFragmentViewHolder(mLayoutInflater.inflate(R.layout.topic_fragment_item,null));
+    }
+
+    @Override
+    public void onBindViewHolder(TopicFragmentViewHolder holder, int position) {
+        //设置数据
+        TopicFragmentBean.DataBean.ItemsBean itemsBean = items.get(position);
+        holder.topicFragmentItemTvName.setText(itemsBean.getTopic_name());
+
+        Picasso.with(mContext)
+                .load(itemsBean.getCover_img())
+                .into(holder.topicFragmentItemIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items == null ? 0 : items.size();
+    }
+
+
+    static class TopicFragmentViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.topic_fragment_item_iv)
+        ImageView topicFragmentItemIv;
+        @BindView(R.id.topic_fragment_item_tvName)
+        TextView topicFragmentItemTvName;
+
+        TopicFragmentViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/example/pc/imitationliangcang/ui/fragment/shop_fragment_detail/TopicFragment.java
+++ app/src/main/java/com/example/pc/imitationliangcang/ui/fragment/shop_fragment_detail/TopicFragment.java
@@ -1,15 +1,32 @@
 package com.example.pc.imitationliangcang.ui.fragment.shop_fragment_detail;
 
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.pc.imitationliangcang.R;
 import com.example.pc.imitationliangcang.base.BaseFragment;
+import com.example.pc.imitationliangcang.bean.TopicFragmentBean;
+import com.example.pc.imitationliangcang.common.NetWorkUrl;
+import com.example.pc.imitationliangcang.ui.adapter.shopfragment.TopicFragmentRecyclerVIewAdapter;
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by PC on 2017/7/6.
  */
 
 public class TopicFragment extends BaseFragment {
+
+    TopicFragmentRecyclerVIewAdapter adapter;
+
+    @BindView(R.id.topic_fragment_recyclerView)
+    RecyclerView topicFragmentRecyclerView;
+
     @Override
     public int getLayoutID() {
         return R.layout.shop_fragment_topic_fragment;
@@ -20,7 +37,37 @@ public class TopicFragment extends BaseFragment {
     }
 
     @Override
-    public void initData() {
+    public void processData(String s) {
+        super.processData(s);
 
+        Log.e("TAG","TopicFragment的数据==="+s);
+
+        if (!TextUtils.isEmpty(s)) {
+            //解析数据
+            TopicFragmentBean topicFragmentBean = new Gson().fromJson(s, TopicFragmentBean.class);
+            List<TopicFragmentBean.DataBean.ItemsBean> items = topicFragmentBean.getData().getItems();
+
+            //设置布局及适配器
+            LinearLayoutManager manager = new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false);
+            topicFragmentRecyclerView.setLayoutManager(manager);
+
+            adapter = new TopicFragmentRecyclerVIewAdapter(mContext,items);
+            topicFragmentRecyclerView.setAdapter(adapter);
+        }
+
+
+
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
+
+
+    @Override
+    public String getUrl() {
+        return NetWorkUrl.TOPICFRAGMENTURL;
     }
 }
